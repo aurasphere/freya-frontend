fc26fe2f8838101e9a658c2b2eaf3a5ee90a2df9
Donato Rimenti <donatohan.rimenti@gmail.com>
2019-10-23 23:45:59 +0200

    Minor refactoring. Not working atm.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { ListComponent } from '../list.component';
-import { AppComponent } from './app.component';
-import { DetailsComponent } from '../details.component';
-import { LoginComponent } from '../pages/login/login.component';
+import { ListComponent } from './list.component';
+import { AppComponent } from './template/app.component';
+import { DetailsComponent } from './details.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthService } from '../services/auth.service';
-import { AuthInterceptor } from '../misc/auth.interceptor';
+import { AuthService } from './services/auth.service';
+import { AuthInterceptor } from './misc/auth.interceptor';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
@@ -25,7 +26,7 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
     RouterModule.forRoot([
       { path: 'dettagli/:itemId', component: DetailsComponent},
       { path: 'login', component: LoginComponent },
-	  { path: '', component: ListComponent}
+	  { path: '', component: AppComponent}
     ])
   ],
   providers: [AuthService,
---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+	username;
+	password;
+	showErrorMessage = false;
+	loginForm;
+	
+	constructor(private auth: AuthService, private router: Router, private formBuilder: FormBuilder) {
+	this.loginForm = this.formBuilder.group({
+      username: '',
+	  password: ''
+    });
+	}
+	
+	doLogin(form) {
+		this.auth.doLogin(form.username, form.password)
+		  .subscribe(response => {
+			if (response.status === 401) {
+				this.showErrorMessage = true;
+			} else {
+				this.router.navigate(['']);
+			}	
+		  });
+	}
+ 
+}
